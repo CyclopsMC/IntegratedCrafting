89ed5a1d0162d1b47209d476deb59acd42361217
Ruben Taelman <rubensworks@gmail.com>
2018-12-23 11:39:52 +0100

    Fix PendingCraftingJobResultIndexObserver not checking channels

--- src/main/java/org/cyclops/integratedcrafting/core/PendingCraftingJobResultIndexObserver.java
+++ src/main/java/org/cyclops/integratedcrafting/core/PendingCraftingJobResultIndexObserver.java
@@ -12,6 +12,7 @@ import org.cyclops.cyclopscore.ingredient.collection.IngredientCollectionPrototy
 import org.cyclops.cyclopscore.ingredient.storage.IngredientComponentStorageCollectionWrapper;
 import org.cyclops.integratedcrafting.api.crafting.CraftingJob;
 import org.cyclops.integrateddynamics.api.ingredient.IIngredientComponentStorageObservable;
+import org.cyclops.integrateddynamics.api.network.IPositionedAddonsNetwork;
 
 import java.util.Iterator;
 import java.util.List;
@@ -47,69 +48,73 @@ public class PendingCraftingJobResultIndexObserver<T, M>
             ObjectIterator<Int2ObjectMap.Entry<Map<IngredientComponent<?, ?>, List<IPrototypedIngredient<?, ?>>>>> jobEntryIt = processingJobs.int2ObjectEntrySet().iterator();
             while (jobEntryIt.hasNext()) {
                 Int2ObjectMap.Entry<Map<IngredientComponent<?, ?>, List<IPrototypedIngredient<?, ?>>>> jobEntry = jobEntryIt.next();
-                List<IPrototypedIngredient<?, ?>> pendingIngredientsUnsafe = jobEntry.getValue().get(ingredientComponent);
-                if (pendingIngredientsUnsafe != null) {
-                    // Remove pending ingredients that were added in the event
-                    List<IPrototypedIngredient<T, M>> pendingIngredients = (List<IPrototypedIngredient<T, M>>) (Object) pendingIngredientsUnsafe;
+                // Only check jobs that have a matching channel with the event
+                CraftingJob craftingJob = handler.getAllCraftingJobs().get(jobEntry.getIntKey());
+                if (craftingJob.getChannel() == IPositionedAddonsNetwork.WILDCARD_CHANNEL || craftingJob.getChannel() == event.getChannel()) {
+                    List<IPrototypedIngredient<?, ?>> pendingIngredientsUnsafe = jobEntry.getValue().get(ingredientComponent);
+                    if (pendingIngredientsUnsafe != null) {
+                        // Remove pending ingredients that were added in the event
+                        List<IPrototypedIngredient<T, M>> pendingIngredients = (List<IPrototypedIngredient<T, M>>) (Object) pendingIngredientsUnsafe;
 
-                    // Iterate over all pending ingredients for this ingredient component
-                    ListIterator<IPrototypedIngredient<T, M>> it = pendingIngredients.listIterator();
-                    while (it.hasNext()) {
-                        IPrototypedIngredient<T, M> prototypedIngredient = it.next();
-                        final long initialQuantity = matcher.getQuantity(prototypedIngredient.getPrototype());
-                        long remainingQuantity = initialQuantity;
+                        // Iterate over all pending ingredients for this ingredient component
+                        ListIterator<IPrototypedIngredient<T, M>> it = pendingIngredients.listIterator();
+                        while (it.hasNext()) {
+                            IPrototypedIngredient<T, M> prototypedIngredient = it.next();
+                            final long initialQuantity = matcher.getQuantity(prototypedIngredient.getPrototype());
+                            long remainingQuantity = initialQuantity;
 
-                        // Lazily create ingredientsHayStack only when needed,
-                        // because we need to copy all ingredients from addedIngredients,
-                        // which can get expensive
-                        // We need to make a copy because multiple crafting jobs can have the same pending instances,
-                        // so each instance may only be consumed by a single crafting job.
-                        if (ingredientsHayStack == null) {
-                            if (addedIngredients.contains(prototypedIngredient.getPrototype(),
-                                    prototypedIngredient.getCondition())) {
-                                IngredientCollectionPrototypeMap<T, M> prototypeMap = new IngredientCollectionPrototypeMap<>(ingredientComponent);
-                                ingredientsHayStack = new IngredientComponentStorageCollectionWrapper<>(prototypeMap);
-                                prototypeMap.addAll(addedIngredients);
-                            } else {
-                                continue;
+                            // Lazily create ingredientsHayStack only when needed,
+                            // because we need to copy all ingredients from addedIngredients,
+                            // which can get expensive
+                            // We need to make a copy because multiple crafting jobs can have the same pending instances,
+                            // so each instance may only be consumed by a single crafting job.
+                            if (ingredientsHayStack == null) {
+                                if (addedIngredients.contains(prototypedIngredient.getPrototype(),
+                                        prototypedIngredient.getCondition())) {
+                                    IngredientCollectionPrototypeMap<T, M> prototypeMap = new IngredientCollectionPrototypeMap<>(ingredientComponent);
+                                    ingredientsHayStack = new IngredientComponentStorageCollectionWrapper<>(prototypeMap);
+                                    prototypeMap.addAll(addedIngredients);
+                                } else {
+                                    continue;
+                                }
+                            }
+
+                            // Iteratively extract the pending ingredient from the hay stack.
+                            T extracted;
+                            do {
+                                extracted = ingredientsHayStack.extract(prototypedIngredient.getPrototype(),
+                                        prototypedIngredient.getCondition(), false);
+
+                                if (matcher.isEmpty(extracted)) {
+                                    // Quickly break when no matches are available anymore
+                                    break;
+                                }
+
+                                remainingQuantity -= matcher.getQuantity(extracted);
+                            } while (!matcher.isEmpty(extracted) && remainingQuantity > 0);
+
+                            // Update the list if the prototype has changed.
+                            if (remainingQuantity <= 0) {
+                                it.remove();
+                            } else if (initialQuantity != remainingQuantity) {
+                                it.set(new PrototypedIngredient<>(ingredientComponent,
+                                        matcher.withQuantity(prototypedIngredient.getPrototype(), remainingQuantity),
+                                        prototypedIngredient.getCondition()));
                             }
                         }
 
-                        // Iteratively extract the pending ingredient from the hay stack.
-                        T extracted;
-                        do {
-                            extracted = ingredientsHayStack.extract(prototypedIngredient.getPrototype(),
-                                    prototypedIngredient.getCondition(), false);
+                        // If no prototypes for this component type for this crafting job are pending.
+                        if (pendingIngredients.isEmpty()) {
+                            // Remove observer (in next tick) when all pending ingredients are resolved
+                            handler.getObserversPendingDeletion().add(ingredientComponent);
 
-                            if (matcher.isEmpty(extracted)) {
-                                // Quickly break when no matches are available anymore
-                                break;
+                            // Remove crafting job if needed.
+                            jobEntry.getValue().remove(ingredientComponent);
+                            if (jobEntry.getValue().isEmpty()) {
+                                handler.onCraftingJobFinished(handler.getAllCraftingJobs().get(jobEntry.getIntKey()));
+                                handler.getProcessingCraftingJobsRaw().remove(jobEntry.getIntKey());
+                                jobEntryIt.remove();
                             }
-
-                            remainingQuantity -= matcher.getQuantity(extracted);
-                        } while (!matcher.isEmpty(extracted) && remainingQuantity > 0);
-
-                        // Update the list if the prototype has changed.
-                        if (remainingQuantity <= 0) {
-                            it.remove();
-                        } else if (initialQuantity != remainingQuantity) {
-                            it.set(new PrototypedIngredient<>(ingredientComponent,
-                                    matcher.withQuantity(prototypedIngredient.getPrototype(), remainingQuantity),
-                                    prototypedIngredient.getCondition()));
-                        }
-                    }
-
-                    // If no prototypes for this component type for this crafting job are pending.
-                    if (pendingIngredients.isEmpty()) {
-                        // Remove observer (in next tick) when all pending ingredients are resolved
-                        handler.getObserversPendingDeletion().add(ingredientComponent);
-
-                        // Remove crafting job if needed.
-                        jobEntry.getValue().remove(ingredientComponent);
-                        if (jobEntry.getValue().isEmpty()) {
-                            handler.onCraftingJobFinished(handler.getAllCraftingJobs().get(jobEntry.getIntKey()));
-                            handler.getProcessingCraftingJobsRaw().remove(jobEntry.getIntKey());
-                            jobEntryIt.remove();
                         }
                     }
                 }
