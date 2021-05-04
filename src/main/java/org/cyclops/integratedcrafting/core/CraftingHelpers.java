CyclopsMC/IntegratedCrafting

commit 0fbbc3c1b0ad3657c89dc3cba9b9b08304e55260

Fix rare cases where overlapping jobs would cause halting
If a job would cause items to be added to the network,
and another job would consume those items in parallel,
the outputs of the first job could be consumed by the second job,
so that the first job sometimes would not be marked as completed.

The solution involved not allowing new crafting jobs if the observer
knows that it still needs to forcefully observe certain parts of the network.
This would be the case when another job inserts items as output into the network.

Closes #48

Small example of the problem:
Problem occurs because we already had instances in the network!
* storageidx = 10
* craft +6
* insert +3 (actual = 13)
* detect +3
* storageidx = 13
* insert +3 (actual = 16, but idx not updated yet)
* craft X and immediately consume -6 (actual = 10)
* detect -3
* storageidx = 10
==> craft +6 never finishes, because the last +3 was never detected!!!

src/main/java/org/cyclops/integratedcrafting/core/CraftingHelpers.java
@@ -118,7 +118,9 @@ public class CraftingHelpers {
                                                                              IngredientComponent<T, M> ingredientComponent,
                                                                              boolean scheduleObservation) {
         IPositionedAddonsNetworkIngredients<T, M> ingredientsNetwork = getIngredientsNetwork(network, ingredientComponent);
-        if (ingredientsNetwork != null) {
+        // Checking isObservationForcedPending ensures that we don't allow crafting jobs
+        // if the network is guaranteed to have uncommitted changes, such as the one in #48
+        if (ingredientsNetwork != null && !ingredientsNetwork.isObservationForcedPending(channel)) {
             if (scheduleObservation) {
                 ingredientsNetwork.scheduleObservation();
             }

commit 34cf8c1a78d708d5979a915c6584a530b22ab9af

Fix job calculations sometimes containing invalid air, Closes #56
This could occur during frequent network changes.

This was caused by the change in 0fbbc3c1b0ad3657c89dc3cba9b9b08304e55260
This modifies that commit, and implements a better solution.

Concretely, instead of silently failing partial job calculation,
we forcefully run all observers synchronously if needed,
to ensure consistent indexes.

(This used to be solved by abe290f7560a29d931be84175eb7f00a7cc4480d,
but this caused job calc to take too long)

build.properties
@@ -4,7 +4,7 @@ forge_version=36.0.14
 mcp_mappings_channel=snapshot
 mcp_mappings_version=20201028-1.16.3
 cyclopscore_version=1.11.1-35
-integrateddynamics_version=1.16.5-1.8.0-76
+integrateddynamics_version=1.16.5-1.8.0-79
 release_type=release
 fingerprint=bd0353b3e8a2810d60dd584e256e364bc3bedd44
 

src/main/java/org/cyclops/integratedcrafting/core/CraftingHelpers.java
@@ -144,9 +144,7 @@ public class CraftingHelpers {
                                                                              IngredientComponent<T, M> ingredientComponent,
                                                                              boolean scheduleObservation) {
         IPositionedAddonsNetworkIngredients<T, M> ingredientsNetwork = getIngredientsNetwork(network, ingredientComponent).orElse(null);
-        // Checking isObservationForcedPending ensures that we don't allow crafting jobs
-        // if the network is guaranteed to have uncommitted changes, such as the one in #48
-        if (ingredientsNetwork != null && !ingredientsNetwork.isObservationForcedPending(channel)) {
+        if (ingredientsNetwork != null) {
             if (scheduleObservation) {
                 ingredientsNetwork.scheduleObservation();
             }
@@ -216,6 +214,16 @@ public class CraftingHelpers {
         ICraftingNetwork craftingNetwork = getCraftingNetworkChecked(network);
         IRecipeIndex recipeIndex = craftingNetwork.getRecipeIndex(channel);
         Function<IngredientComponent<?, ?>, IIngredientComponentStorage> storageGetter = getNetworkStorageGetter(network, channel, true);
+
+        // If the network is guaranteed to have uncommitted changes (such as the one in #48),
+        // forcefully run observers synchronously, so that we can calculate the job in a consistent network state.
+        for (IngredientComponent<?, ?> ingredientComponent : IngredientComponent.REGISTRY.getValues()) {
+            IPositionedAddonsNetworkIngredients<?, ?> ingredientsNetwork = getIngredientsNetwork(network, ingredientComponent).orElse(null);
+            if (ingredientsNetwork != null && (ingredientsNetwork.isObservationForcedPending(channel) || Math.random() > 0.5)) {
+                ingredientsNetwork.runObserverSync();
+            }
+        }
+
         PartialCraftingJobCalculation result = calculateCraftingJobs(recipeIndex, channel, storageGetter, recipe, amount,
                 craftMissing, Maps.newIdentityHashMap(), identifierGenerator, craftingJobsGraph, Sets.newHashSet(),
                 collectMissingRecipes);