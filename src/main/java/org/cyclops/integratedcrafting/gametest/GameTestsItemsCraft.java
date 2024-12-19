364c006aad3a139c29a68062684791d632a4aec9
Ruben Taelman <rubensworks@gmail.com>
2024-12-19 15:20:29 +0100

    Add game test for issue #1432

--- src/main/java/org/cyclops/integratedcrafting/gametest/GameTestsItemsCraft.java
+++ src/main/java/org/cyclops/integratedcrafting/gametest/GameTestsItemsCraft.java
@@ -1,5 +1,7 @@
 package org.cyclops.integratedcrafting.gametest;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.gametest.framework.GameTest;
@@ -14,6 +16,14 @@ import net.minecraft.world.level.block.entity.ChestBlockEntity;
 import net.neoforged.neoforge.gametest.GameTestHolder;
 import net.neoforged.neoforge.gametest.PrefixGameTestTemplate;
 import org.apache.commons.lang3.tuple.Triple;
+import org.cyclops.commoncapabilities.IngredientComponents;
+import org.cyclops.commoncapabilities.api.capability.itemhandler.ItemMatch;
+import org.cyclops.commoncapabilities.api.capability.recipehandler.IPrototypedIngredientAlternatives;
+import org.cyclops.commoncapabilities.api.capability.recipehandler.PrototypedIngredientAlternativesList;
+import org.cyclops.commoncapabilities.api.capability.recipehandler.RecipeDefinition;
+import org.cyclops.commoncapabilities.api.ingredient.IngredientComponent;
+import org.cyclops.commoncapabilities.api.ingredient.MixedIngredients;
+import org.cyclops.commoncapabilities.api.ingredient.PrototypedIngredient;
 import org.cyclops.integratedcrafting.Reference;
 import org.cyclops.integratedcrafting.part.PartTypes;
 import org.cyclops.integratedcrafting.part.aspect.CraftingAspectWriteBuilders;
@@ -21,10 +31,16 @@ import org.cyclops.integratedcrafting.part.aspect.CraftingAspects;
 import org.cyclops.integrateddynamics.api.part.PartPos;
 import org.cyclops.integrateddynamics.api.part.write.IPartStateWriter;
 import org.cyclops.integrateddynamics.core.block.IgnoredBlockStatus;
+import org.cyclops.integrateddynamics.core.evaluate.variable.ValueObjectTypeRecipe;
 import org.cyclops.integrateddynamics.core.evaluate.variable.ValueTypeBoolean;
+import org.cyclops.integrateddynamics.core.evaluate.variable.ValueTypes;
 import org.cyclops.integrateddynamics.core.helper.PartHelpers;
 
+import java.util.List;
+import java.util.Map;
+
 import static org.cyclops.integratedcrafting.gametest.GameTestHelpersIntegratedCrafting.*;
+import static org.cyclops.integrateddynamics.gametest.GameTestHelpersIntegratedDynamics.createVariableForValue;
 
 @GameTestHolder(Reference.MOD_ID)
 @PrefixGameTestTemplate(false)
@@ -49,12 +65,6 @@ public class GameTestsItemsCraft {
         enableRecipeInWriter(helper, positions.writer(), new ItemStack(Items.CHEST));
 
         helper.succeedWhen(() -> {
-            // Check if items have been crafted
-            helper.assertValueEqual(chestIn.getItem(0).getItem(), Items.OAK_PLANKS, "Slot 0 item is incorrect");
-            helper.assertValueEqual(chestIn.getItem(0).getCount(), 56, "Slot 0 amount is incorrect");
-            helper.assertValueEqual(chestIn.getItem(1).getItem(), Items.CHEST, "Slot 1 item is incorrect");
-            helper.assertValueEqual(chestIn.getItem(1).getCount(), 1, "Slot 1 amount is incorrect");
-
             // Check crafting interface state
             helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(0), "Recipe in crafting interface is not valid");
 
@@ -68,6 +78,12 @@ public class GameTestsItemsCraft {
             );
             helper.assertValueEqual(partStateWriter.getActiveAspect(), CraftingAspects.Write.ITEMSTACK_CRAFT, "Active aspect is incorrect");
             helper.assertTrue(partStateWriter.getErrors(CraftingAspects.Write.ITEMSTACK_CRAFT).isEmpty(), "Active aspect has errors");
+
+            // Check if items have been crafted
+            helper.assertValueEqual(chestIn.getItem(0).getItem(), Items.OAK_PLANKS, "Slot 0 item is incorrect");
+            helper.assertValueEqual(chestIn.getItem(0).getCount(), 56, "Slot 0 amount is incorrect");
+            helper.assertValueEqual(chestIn.getItem(1).getItem(), Items.CHEST, "Slot 1 item is incorrect");
+            helper.assertValueEqual(chestIn.getItem(1).getCount(), 1, "Slot 1 amount is incorrect");
         });
     }
 
@@ -89,12 +105,6 @@ public class GameTestsItemsCraft {
         setWriterAspectProperty(positions.writer(), CraftingAspects.Write.ITEMSTACK_CRAFT, CraftingAspectWriteBuilders.PROP_IGNORE_STORAGE, ValueTypeBoolean.ValueBoolean.of(true));
 
         helper.succeedWhen(() -> {
-            // Check if items have been crafted
-            helper.assertValueEqual(chestIn.getItem(0).getItem(), Items.CHEST, "Slot 0 item is incorrect");
-            helper.assertValueEqual(chestIn.getItem(0).getCount(), 1, "Slot 0 amount is incorrect");
-            helper.assertValueEqual(chestIn.getItem(1).getItem(), Items.CHEST, "Slot 1 item is incorrect");
-            helper.assertValueEqual(chestIn.getItem(1).getCount(), 7, "Slot 1 amount is incorrect");
-
             // Check crafting interface state
             helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(0), "Recipe in crafting interface is not valid");
 
@@ -108,6 +118,12 @@ public class GameTestsItemsCraft {
             );
             helper.assertValueEqual(partStateWriter.getActiveAspect(), CraftingAspects.Write.ITEMSTACK_CRAFT, "Active aspect is incorrect");
             helper.assertTrue(partStateWriter.getErrors(CraftingAspects.Write.ITEMSTACK_CRAFT).isEmpty(), "Active aspect has errors");
+
+            // Check if items have been crafted
+            helper.assertValueEqual(chestIn.getItem(0).getItem(), Items.CHEST, "Slot 0 item is incorrect");
+            helper.assertValueEqual(chestIn.getItem(0).getCount(), 1, "Slot 0 amount is incorrect");
+            helper.assertValueEqual(chestIn.getItem(1).getItem(), Items.CHEST, "Slot 1 item is incorrect");
+            helper.assertValueEqual(chestIn.getItem(1).getCount(), 7, "Slot 1 amount is incorrect");
         });
     }
 
@@ -126,13 +142,59 @@ public class GameTestsItemsCraft {
         enableRecipeInWriter(helper, positions.writer(), new ItemStack(Items.IRON_INGOT));
 
         helper.succeedWhen(() -> {
+            // Check crafting interface state
+            helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(0), "Recipe in crafting interface is not valid");
+
             // Check if items have been crafted
             helper.assertValueEqual(chestIn.getItem(0).getItem(), Items.IRON_INGOT, "Slot 0 item is incorrect");
             helper.assertValueEqual(chestIn.getItem(0).getCount(), 1, "Slot 0 amount is incorrect");
             helper.assertValueEqual(chestIn.getItem(1).getCount(), 0, "Slot 1 amount is incorrect");
+        });
+    }
 
+    @GameTest(template = TEMPLATE_EMPTY, timeoutTicks = TIMEOUT)
+    public void testItemsCraftIronIngotRecipeWithEmptySpaces(GameTestHelper helper) {
+        GameTestHelpersIntegratedCrafting.NetworkPositions positions = createBasicNetwork(helper, POS, Blocks.FURNACE);
+
+        // Insert items in interface chest
+        ChestBlockEntity chestIn = helper.getBlockEntity(POS.east());
+        chestIn.setItem(0, new ItemStack(Items.RAW_IRON, 1));
+
+        // Add iron ingot recipe with spaces to furnace
+        Map<IngredientComponent<?, ?>, List<IPrototypedIngredientAlternatives<?, ?>>> recipeIn = Maps.newIdentityHashMap();
+        List<IPrototypedIngredientAlternatives<?, ?>> alternatives = Lists.newArrayList();
+        Map<IngredientComponent<?, ?>, List<?>> recipeOut = Maps.newIdentityHashMap();
+        alternatives.add(new PrototypedIngredientAlternativesList<>(Lists.newArrayList(
+                new PrototypedIngredient<>(IngredientComponents.ITEMSTACK, ItemStack.EMPTY, ItemMatch.ITEM | ItemMatch.DATA)
+        )));
+        alternatives.add(new PrototypedIngredientAlternativesList<>(Lists.newArrayList(
+                new PrototypedIngredient<>(IngredientComponents.ITEMSTACK, ItemStack.EMPTY, ItemMatch.ITEM | ItemMatch.DATA)
+        )));
+        alternatives.add(new PrototypedIngredientAlternativesList<>(Lists.newArrayList(
+                new PrototypedIngredient<>(IngredientComponents.ITEMSTACK, ItemStack.EMPTY, ItemMatch.ITEM | ItemMatch.DATA)
+        )));
+        alternatives.add(new PrototypedIngredientAlternativesList<>(Lists.newArrayList(
+                new PrototypedIngredient<>(IngredientComponents.ITEMSTACK, ItemStack.EMPTY, ItemMatch.ITEM | ItemMatch.DATA)
+        )));
+        alternatives.add(new PrototypedIngredientAlternativesList<>(Lists.newArrayList(
+                new PrototypedIngredient<>(IngredientComponents.ITEMSTACK, new ItemStack(Items.RAW_IRON), ItemMatch.ITEM | ItemMatch.DATA)
+        )));
+        recipeIn.put(IngredientComponents.ITEMSTACK, alternatives);
+        recipeOut.put(IngredientComponents.ITEMSTACK, Lists.newArrayList(new ItemStack(Items.IRON_INGOT)));
+        ItemStack variableRecipe = createVariableForValue(helper.getLevel(), ValueTypes.OBJECT_RECIPE, ValueObjectTypeRecipe.ValueRecipe.of(new RecipeDefinition(recipeIn, new MixedIngredients(recipeOut))));
+        positions.interfaceStates().get(0).getInventoryVariables().setItem(0, variableRecipe);
+
+        // Enable crafting aspect in crafting writer
+        enableRecipeInWriter(helper, positions.writer(), new ItemStack(Items.IRON_INGOT));
+
+        helper.succeedWhen(() -> {
             // Check crafting interface state
             helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(0), "Recipe in crafting interface is not valid");
+
+            // Check if items have been crafted
+            helper.assertValueEqual(chestIn.getItem(0).getItem(), Items.IRON_INGOT, "Slot 0 item is incorrect");
+            helper.assertValueEqual(chestIn.getItem(0).getCount(), 1, "Slot 0 amount is incorrect");
+            helper.assertValueEqual(chestIn.getItem(1).getCount(), 0, "Slot 1 amount is incorrect");
         });
     }
 
@@ -152,14 +214,14 @@ public class GameTestsItemsCraft {
         enableRecipeInWriter(helper, positions.writer(), new ItemStack(Items.CHEST));
 
         helper.succeedWhen(() -> {
+            // Check crafting interface state
+            helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(0), "Recipe 0 in crafting interface is not valid");
+            helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(1), "Recipe 1 in crafting interface is not valid");
+
             // Check if items have been crafted
             helper.assertValueEqual(chestIn.getItem(0).getItem(), Items.CHEST, "Slot 1 item is incorrect");
             helper.assertValueEqual(chestIn.getItem(0).getCount(), 1, "Slot 1 amount is incorrect");
             helper.assertValueEqual(chestIn.getItem(1).getCount(), 0, "Slot 1 amount is incorrect");
-
-            // Check crafting interface state
-            helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(0), "Recipe in crafting interface is not valid");
-            helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(1), "Recipe in crafting interface is not valid");
         });
     }
 
@@ -182,17 +244,17 @@ public class GameTestsItemsCraft {
         enableRecipeInWriter(helper, positions.writer(), new ItemStack(Items.IRON_INGOT, 5));
 
         helper.succeedWhen(() -> {
+            // Check crafting interface state
+            helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(0), "Recipe in crafting interface 0 is not valid");
+            helper.assertTrue(positions.interfaceStates().get(1).isRecipeSlotValid(0), "Recipe in crafting interface 1 is not valid");
+            helper.assertTrue(positions.interfaceStates().get(2).isRecipeSlotValid(0), "Recipe in crafting interface 2 is not valid");
+            helper.assertTrue(positions.interfaceStates().get(3).isRecipeSlotValid(0), "Recipe in crafting interface 3 is not valid");
+            helper.assertTrue(positions.interfaceStates().get(4).isRecipeSlotValid(0), "Recipe in crafting interface 4 is not valid");
+
             // Check if items have been crafted
             helper.assertValueEqual(chestIn.getItem(0).getItem(), Items.IRON_INGOT, "Slot 0 item is incorrect");
             helper.assertValueEqual(chestIn.getItem(0).getCount(), 5, "Slot 0 amount is incorrect");
             helper.assertValueEqual(chestIn.getItem(1).getCount(), 0, "Slot 1 amount is incorrect");
-
-            // Check crafting interface state
-            helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(0), "Recipe in crafting interface is not valid");
-            helper.assertTrue(positions.interfaceStates().get(1).isRecipeSlotValid(0), "Recipe in crafting interface is not valid");
-            helper.assertTrue(positions.interfaceStates().get(2).isRecipeSlotValid(0), "Recipe in crafting interface is not valid");
-            helper.assertTrue(positions.interfaceStates().get(3).isRecipeSlotValid(0), "Recipe in crafting interface is not valid");
-            helper.assertTrue(positions.interfaceStates().get(4).isRecipeSlotValid(0), "Recipe in crafting interface is not valid");
 
             helper.assertBlockProperty(POS.west(), AbstractFurnaceBlock.LIT, true);
             helper.assertBlockProperty(POS.south().west(), AbstractFurnaceBlock.LIT, true);
@@ -228,17 +290,17 @@ public class GameTestsItemsCraft {
         setCraftingInterfaceBlockingMode(positions.interfaces().get(4), false);
 
         helper.succeedWhen(() -> {
+            // Check crafting interface state
+            helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(0), "Recipe in crafting interface 0 is not valid");
+            helper.assertTrue(positions.interfaceStates().get(1).isRecipeSlotValid(0), "Recipe in crafting interface 1 is not valid");
+            helper.assertTrue(positions.interfaceStates().get(2).isRecipeSlotValid(0), "Recipe in crafting interface 2 is not valid");
+            helper.assertTrue(positions.interfaceStates().get(3).isRecipeSlotValid(0), "Recipe in crafting interface 3 is not valid");
+            helper.assertTrue(positions.interfaceStates().get(4).isRecipeSlotValid(0), "Recipe in crafting interface 4 is not valid");
+
             // Check if items have been crafted
             helper.assertValueEqual(chestIn.getItem(0).getItem(), Items.IRON_INGOT, "Slot 0 item is incorrect");
             helper.assertValueEqual(chestIn.getItem(0).getCount(), 10, "Slot 0 amount is incorrect");
             helper.assertValueEqual(chestIn.getItem(1).getCount(), 0, "Slot 1 amount is incorrect");
-
-            // Check crafting interface state
-            helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(0), "Recipe in crafting interface is not valid");
-            helper.assertTrue(positions.interfaceStates().get(1).isRecipeSlotValid(0), "Recipe in crafting interface is not valid");
-            helper.assertTrue(positions.interfaceStates().get(2).isRecipeSlotValid(0), "Recipe in crafting interface is not valid");
-            helper.assertTrue(positions.interfaceStates().get(3).isRecipeSlotValid(0), "Recipe in crafting interface is not valid");
-            helper.assertTrue(positions.interfaceStates().get(4).isRecipeSlotValid(0), "Recipe in crafting interface is not valid");
 
             helper.assertBlockProperty(POS.west(), AbstractFurnaceBlock.LIT, true);
             helper.assertBlockProperty(POS.south().west(), AbstractFurnaceBlock.LIT, true);
@@ -275,17 +337,17 @@ public class GameTestsItemsCraft {
         enableRecipeInWriter(helper, positions.writer(), new ItemStack(Items.CRAFTER));
 
         helper.succeedWhen(() -> {
+            // Check crafting interface state
+            helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(0), "Recipe 0 in crafting interface 0 is not valid");
+            helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(1), "Recipe 1 in crafting interface 0 is not valid");
+            helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(2), "Recipe 2 in crafting interface 0 is not valid");
+            helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(3), "Recipe 3 in crafting interface 0 is not valid");
+            helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(4), "Recipe 4 in crafting interface 0 is not valid");
+            helper.assertTrue(positions.interfaceStates().get(1).isRecipeSlotValid(0), "Recipe 0 in crafting interface 1 is not valid");
+
             // Check if items have been crafted
             helper.assertValueEqual(chestIn.getItem(0).getItem(), Items.CRAFTER, "Slot 0 item is incorrect");
             helper.assertValueEqual(chestIn.getItem(0).getCount(), 1, "Slot 0 amount is incorrect");
-
-            // Check crafting interface state
-            helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(0), "Recipe in crafting interface is not valid");
-            helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(1), "Recipe in crafting interface is not valid");
-            helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(2), "Recipe in crafting interface is not valid");
-            helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(3), "Recipe in crafting interface is not valid");
-            helper.assertTrue(positions.interfaceStates().get(0).isRecipeSlotValid(4), "Recipe in crafting interface is not valid");
-            helper.assertTrue(positions.interfaceStates().get(1).isRecipeSlotValid(0), "Recipe in crafting interface is not valid");
         });
     }
 
