28ac7f0595c95b7cdc0d768cdc882d264cec2880
Ruben Taelman <rubensworks@gmail.com>
2025-12-30 10:45:54 +0100

    Fix re-insertion during job scheduling happening during missing ingred collection

--- src/main/java/org/cyclops/integratedcrafting/core/CraftingHelpers.java
+++ src/main/java/org/cyclops/integratedcrafting/core/CraftingHelpers.java
@@ -1181,7 +1181,7 @@ public class CraftingHelpers {
 
             // If none of the alternatives were found, fail immediately
             if (!hasInputInstance) {
-                if (!simulate) {
+                if (!simulate && !collectMissingIngredients) {
                     // But first, re-insert all already-extracted instances
                     for (T instance : inputInstances) {
                         T remaining = storage.insert(instance, false);
@@ -1191,7 +1191,6 @@ public class CraftingHelpers {
                                     "and non-simulation: " + storage + ". Lost: " + remaining);
                         }
                     }
-                    inputInstances.clear();
                 }
 
                 if (!collectMissingIngredients) {
