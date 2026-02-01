21cb68c10fa87469c0bc1d03b6e8004707a692b7
Ruben Taelman <rubensworks@gmail.com>
2026-02-01 14:22:20 +0100

    Fix crash due to incorrect Lists import, Closes #176

--- src/main/java/org/cyclops/integratedcrafting/api/crafting/CraftingJob.java
+++ src/main/java/org/cyclops/integratedcrafting/api/crafting/CraftingJob.java
@@ -1,12 +1,12 @@
 package org.cyclops.integratedcrafting.api.crafting;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
 import it.unimi.dsi.fastutil.ints.IntList;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.IntArrayTag;
 import net.minecraft.nbt.Tag;
-import org.apache.commons.compress.utils.Lists;
 import org.cyclops.commoncapabilities.api.capability.recipehandler.IRecipeDefinition;
 import org.cyclops.commoncapabilities.api.ingredient.IIngredientMatcher;
 import org.cyclops.commoncapabilities.api.ingredient.IMixedIngredients;
