ee2d3694ea1d26c4105b44781ce2943ee063419e
Ruben Taelman <rubensworks@gmail.com>
2026-08-06 08:53:49 +0200

    Fix item dupe from replacing crafting interfaces with crafting buffers, Closes #212

--- src/main/java/org/cyclops/integratedcrafting/core/part/PartTypeInterfaceCraftingBase.java
+++ src/main/java/org/cyclops/integratedcrafting/core/part/PartTypeInterfaceCraftingBase.java
@@ -1,6 +1,7 @@
 package org.cyclops.integratedcrafting.core.part;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
@@ -13,6 +14,7 @@ import net.minecraftforge.common.util.LazyOptional;
 import org.cyclops.commoncapabilities.api.ingredient.IPrototypedIngredient;
 import org.cyclops.commoncapabilities.api.ingredient.IngredientComponent;
 import org.cyclops.commoncapabilities.api.ingredient.IngredientInstanceWrapper;
+import org.cyclops.commoncapabilities.api.ingredient.MixedIngredients;
 import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorage;
 import org.cyclops.integratedcrafting.GeneralConfig;
 import org.cyclops.integratedcrafting.api.crafting.CraftingJob;
@@ -179,6 +181,7 @@ public abstract class PartTypeInterfaceCraftingBase<P extends PartTypeInterfaceC
             for (ItemStack instance : craftingJob.getIngredientsStorageBuffer().getInstances(IngredientComponent.ITEMSTACK)) {
                 itemStacks.add(instance);
             }
+            craftingJob.setIngredientsStorageBuffer(new MixedIngredients(Maps.newIdentityHashMap()));
         }
 
         super.addDrops(target, state, itemStacks, dropMainElement, saveState);
